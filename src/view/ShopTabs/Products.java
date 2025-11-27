464bbff2ef4023675b48ed206013d5725aadf66c
Bionix-OSE <10423123@student.vgu.edu.vn>
2025-11-27 22:48:34 +0700

    20251127-2248: Make ShopUI tabs read-only

--- src/view/ShopTabs/Products.java
+++ src/view/ShopTabs/Products.java
@@ -45,9 +45,16 @@ public class Products extends javax.swing.JPanel {
             Class[] types = new Class [] {
                 java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class, java.lang.String.class
             };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false
+            };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
             }
         });
         Table.setColumnSelectionAllowed(true);
